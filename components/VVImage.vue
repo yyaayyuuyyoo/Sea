f15920ef2072ee7f1fd6a6949d5112be6df4fdc3
ooookai <ooookai@gmail.com>
2021-04-14 16:59:53 +0800

    VVImage

---
+++ components/VVImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-image
+    :config="{
+      image: image,
+    }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      image: null,
+    }
+  },
+  computed: {
+    //
+  },
+  mounted() {
+    const image = new window.Image()
+    image.src = this.src
+    image.onload = () => {
+      // set image only when it is loaded
+      this.image = image
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+/*  */
+</style>
